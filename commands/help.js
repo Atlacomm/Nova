6c8153e58d50b341f2d32700503d22a3665aa606
christian thompson <40225101+cjthomp2005@users.noreply.github.com>
2019-05-11 12:04:25 -0500

    added all code so far

---
+++ commands/help.js
@@ -0,0 +1,75 @@
+/************************************************************************************************
+ *...............................................................................
+ *..........%%%%,....%%%%,...,%%%%%%%%/...%%%%#..../%%%%..../%%%%%%..............
+ *.........,%%%%%/...%%%%,..%%%%%%%%%%%%,.(%%%%....%%%%#...,%%%%%%%%.............
+ *.........,%%%%%%%..%%%%,.(%%%%*...%%%%%..%%%%*..,%%%%....%%%%.%%%%*............
+ *.........,%%%%%%%%.%%%%,.%%%%%....%%%%%..*%%%%..%%%%(...*%%%%.,%%%%............
+ *.........,%%%%.%%%%%%%%,.%%%%%....%%%%%...%%%%,.%%%%....%%%%#((%%%%*...........
+ *.........,%%%%..%%%%%%%,.(%%%%*...%%%%%...,%%%#(%%%*...#%%%%%%%%%%%%...........
+ *.........,%%%%...*%%%%%,..%%%%%%%%%%%%,....%%%%%%%%....%%%%,....%%%%(..........
+ *.........,%%%%.....%%%%....,%%%%%%%%*......,%%%%%%,...#%%%%.....#%%%%..........
+ *...............................................................................
+ *
+ *   Command here: Command for Nova
+ *   Copyright (C) 2019 Designed and Programed by Swingin30 and Techlion
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***********************************************************************************************/
+const Discord = require('discord.js');
+const images = require('../images.json');
+const fs = require('fs');
+module.exports.run = async (client, message, args) => {
+  const categories = [];
+  const commands = Array.from(client.commands.keys());
+  const settings = require('../settings.json')
+  commands.forEach(function(x) {
+    if (!categories.includes(client.commands.get(x).help.category)) {
+      categories.push(client.commands.get(x).help.category);
+    }
+  });
+  
+
+  if(message.guild){
+    if (!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return message.reply('ERROR: Suzu doesn\'t have the permission to send embed links please enable them to use the full help.');
+  }
+  const embed = new Discord.RichEmbed()
+    .setAuthor(`Nova Help and on ${client.guilds.size} servers`, client.user.avatarURL)
+    .setThumbnail(`${images.unknown}`)
+    .setDescription('Every command you input into Nova for this server is `' + require('../settings.json').prefix + '`')
+    .setColor(0xE70056)
+    .setFooter('Designed and Programed (with love) by Swingin30, Alee and TechLion Copyright 2019, Licensed with GPL-3.0');
+
+  categories.forEach(function(x) {
+    let cat = '';
+    commands.forEach(function(command) {
+      if (client.commands.get(command).help.category == x) {
+        cat = cat + command + '\n';
+      }
+    });
+    embed.addField(x, cat, true)
+  });
+  message.channel.send({embed})
+};
+
+exports.conf = {
+  aliases: ['h'],
+  guildOnly: false,
+};
+exports.help = {
+  name: 'help',
+  description: 'Displays all the commands or a page with information for 1 command.',
+  usage: 'help (command:command-name)',
+  category: '- Utility Commands',
+};
---
+++ commands/sysmanager.js
@@ -0,0 +1,54 @@
+/************************************************************************************************
+ *...............................................................................
+ *..........%%%%,....%%%%,...,%%%%%%%%/...%%%%#..../%%%%..../%%%%%%..............
+ *.........,%%%%%/...%%%%,..%%%%%%%%%%%%,.(%%%%....%%%%#...,%%%%%%%%.............
+ *.........,%%%%%%%..%%%%,.(%%%%*...%%%%%..%%%%*..,%%%%....%%%%.%%%%*............
+ *.........,%%%%%%%%.%%%%,.%%%%%....%%%%%..*%%%%..%%%%(...*%%%%.,%%%%............
+ *.........,%%%%.%%%%%%%%,.%%%%%....%%%%%...%%%%,.%%%%....%%%%#((%%%%*...........
+ *.........,%%%%..%%%%%%%,.(%%%%*...%%%%%...,%%%#(%%%*...#%%%%%%%%%%%%...........
+ *.........,%%%%...*%%%%%,..%%%%%%%%%%%%,....%%%%%%%%....%%%%,....%%%%(..........
+ *.........,%%%%.....%%%%....,%%%%%%%%*......,%%%%%%,...#%%%%.....#%%%%..........
+ *...............................................................................
+ *
+ *   Command here: Command for Nova
+ *   Copyright (C) 2019 Designed and Programed by Swingin30 and Techlion
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***********************************************************************************************/
+module.exports.run = async (client, msg) => {
+    const Discord = require("discord.js");
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    const images = require("../images.json")
+
+    let embed = new Discord.RichEmbed
+    embed.setDescription("System status")
+    embed.setColor(0x00bfff);
+    embed.setThumbnail(`${images.}`)
+    embed.addField("memory usage", `${Math.round(used * 100) / 100}Mb`)
+    embed.addField("guilds", `${client.guilds.size}`)
+    msg.channel.send({embed})
+    
+};
+
+exports.conf = {
+    aliases: [],
+    guildOnly: false,
+  };
+  exports.help = {
+    name: 'sysmanager',
+    description: 'The sysmanager command',
+    usage: 'sysmanager',
+    category: '- Status Commands',
+  };
