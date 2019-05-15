4c8ada6dfa62a1d1e1acf4524b6e63f52c55a4d3
nayabw <42422512+nayabw@users.noreply.github.com>
2019-05-15 12:17:44 -0500

    actually add ban

---
+++ commands/ban.js
@@ -0,0 +1,55 @@
+/************************************************************************************************
+ *...............................................................................
+ *..........%%%%,....%%%%,...,%%%%%%%%/...%%%%#..../%%%%..../%%%%%%..............
+ *.........,%%%%%/...%%%%,..%%%%%%%%%%%%,.(%%%%....%%%%#...,%%%%%%%%.............
+ *.........,%%%%%%%..%%%%,.(%%%%*...%%%%%..%%%%*..,%%%%....%%%%.%%%%*............
+ *.........,%%%%%%%%.%%%%,.%%%%%....%%%%%..*%%%%..%%%%(...*%%%%.,%%%%............
+ *.........,%%%%.%%%%%%%%,.%%%%%....%%%%%...%%%%,.%%%%....%%%%...%%%%*...........
+ *.........,%%%%..%%%%%%%,.(%%%%*...%%%%%...,%%%#(%%%*...#%%%%....%%%%...........
+ *.........,%%%%...*%%%%%,..%%%%%%%%%%%%,....%%%%%%%%....%%%%,....%%%%(..........
+ *.........,%%%%.....%%%%....,%%%%%%%%*......,%%%%%%,...#%%%%.....#%%%%..........
+ *...............................................................................
+ *
+ *   BAN: Command for Nova
+ *   Copyright (C) 2019 Designed and Programed by Swingin30 and Techlion
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***********************************************************************************************/
+module.exports.run = async (client, msg, args, throwE) => {
+      if (!msg.member.hasPermission("BAN_MEMBERS")){
+        msg.reply("Hold up! You aren't allowed to ban members!");
+        return;
+      }
+      let user = msg.mentions.users.first();
+        const member = msg.guild.member(user);
+        if (member) {
+          member.ban('Ban by ' + msg.author.username + ' using Nova.').then(() => {
+            msg.reply(`Successfully banned ${user.tag}`);
+          }).catch(err => {
+            throwE(err)
+          });
+      }
+};
+
+exports.conf = {
+  aliases: [],
+  guildOnly: true,
+};
+exports.help = {
+  name: 'ban',
+  description: 'The ban command',
+  usage: 'ban @member',
+  category: '- Moderation Commands',
+};
