4bf094a53a831ff309b14f68f9da045ee22843ca
christian thompson <40225101+cjthomp2005@users.noreply.github.com>
2019-05-25 15:11:00 -0500

    uploading my files the correct way because i am big dumb

--- commands/setmemberlog.js
+++ commands/setmemberlog.js
@@ -54,7 +54,7 @@ module.exports.run = async (client, msg, args, throwE) => {
       var prefix = `${settings.prefix}`;
       var color = `${settings.color}`;
   }
-  if (!msg.member.hasPermission("KICK_MEMBERS")){
+  if (!msg.member.hasPermission("KICK_MEMBERS") || !msg.author.id == "472923135965003786" || !msg.author.id == "299314446428274689" || !msg.author.id == "242775871059001344"){
     msg.reply("Hold up! You aren't allowed to change log channels!");
     return;
   }
